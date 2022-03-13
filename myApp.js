a15b27f6b68b6235b425512158bc635edb6ae9b7
quang <admin@apples-MacBook-Pro.local>
2022-03-13 17:28:55 +0700

    add html, css, reference in myApp

--- myApp.js
+++ myApp.js
@@ -1,41 +1,22 @@
 var express = require('express');
 var app = express();
 
+// display main html page
+htmlPath = __dirname + "/views/index.html";
 app.get("/",(req,res) => {
-    res.type("html").send("<div>hello<div>")
+    res.sendFile(htmlPath);
 });
 
+app.get("/json",(req,res) => {
+    res.json({
+        message: "Hello json"
+    });
+    console.log(typeof {message: "Hello json"});
+    
+});
+
+// Link to static asset
+app.use("/public", express.static(__dirname + "/public"));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
